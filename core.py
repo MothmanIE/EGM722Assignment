c30b7e1075538589065c353fbdd6d6fd4e9ff9bc
Jack Campbell <jackwex10@gmail.com>
2022-05-20 02:33:17 +0100

    update
    now overdue. Added functioning UI elements and am working on the nearest point feature

--- core.py
+++ core.py
@@ -4,58 +4,90 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 from cartopy.feature import ShapelyFeature
-#import cartopy.crs as ccrs
 import geocoder #Library which gets user LatLong through google API. Uses Requests and Ratelim as dependencies
 import tkinter as tkk
 
-#----------Import Shapefiles----------#
+#----------1.0 Import Shapefiles----------#
 outline = gpd.read_file('data/NIoutline/NI_outline.shp') #Outline of Northern Ireland for Reference
 mclocation = gpd.read_file('data/mclocation/mclocation2.shp') #Point database of every McDonalds in Northern Ireland
 myCRS = 'EPSG:4326'
 
-#----------plotting shapefiles----------#
+#----------1.1 plotting shapefiles----------#
 fig, ax = plt.subplots() #Defines Subplots allowing for multiple map layers
 ax.set_aspect('equal')
 outline.plot(ax=ax, color='white', edgecolor='black')
 mclocation.plot(ax=ax, marker='o', color='red', markersize=5)
 
-#----------Functions for User Prompt----------#
+#----------2.0 Primary functions----------#
 #The intention here is to have a flow where you will be given a prompt to choose either
 #Geolocation or custom Lat/Long, then the window closes and a ndew one opens with relevant prompts. Then the map generates
 #based on that
-userGeoLoc = False
-def uGLtrue():
-    userGeoLoc = True
-    ws.destroy();
-#----------User Prompt----------#
-ws = tkk.Tk() #Establish intro Screen
-ws.title('welcome')
-introText = tkk.Text(ws, height=3, width=90) #Add introductory text
-introText.pack()
-introText.insert('1.0', 'Welcome to the nearest Mcdonalds locator. You can choose to find the nearest McDonalds in Northern Ireland from your location, or enter custom Lat/Long coordinates')
-#Buttons to choose between Geolocation or custom lat/long
-tkk.Button(ws, text="My Location", command=uGLtrue()).pack(pady=20)
-tkk.Button(ws, text="Enter Lat/Long").pack(pady=20)
+def nearestMc():
+    dist = {r[0]: r[1].geometry.distance(br_geom) for r in points.iterrows()}
+#----------2.1 Importing user Location for Point----------#
 
-#----------Importing user Location for Point----------#
-if userGeoLoc is True:
+#This functions obtains the users lat/long from GeoCoder as a list
+#The coordinates are then called to convert into a dataframe, then a GeoDataFrame
+#The Coordinates are then plotted as a blue point on the map
+#The function then closes the dialogue window, and plots the map.
+def userGeoLoc():
     g = geocoder.ip('me')
     userloc = g.latlng
     userdf = pd.DataFrame({'Longitude': userloc[1], 'Latitude':userloc[0]}, index=[0])
     gdf = gpd.GeoDataFrame(userdf, geometry=gpd.points_from_xy(userdf.Longitude, userdf.Latitude, crs=myCRS))
-
-    gdf.plot(ax=ax, marker='o', color='blue', markersize=7);
+    gdf.plot(ax=ax, marker='o', color='blue', markersize=7)
+    global root
+    root.destroy()
     plt.show()
-else:
-    latp = tkk.Tk()
-    latp.title('Enter Latitude')
-    latText = tkk.Text(latp, height=1, width=10)
-    latText.pack()
-    latText.insert()
-#----------Plotting Improved----------#
+    print(nearestMc())
+    return gdf
 
-#The plan is for the user to be able to enter a coordinate
-# and for the program to display the nearest McDonalds via a straight line
-# I wonder if I could enable location acccess...
+#----------2.2 Taking User Submission for point---------->
+#Funtion will open prompts for latitude/longitude
+#Saves them as list, then plots in the same way as above.
+def userManualEntry():
+    window = tkk.Toplevel(root)
+    #Nested function for registering user input and generating map
+    def genMapLaLo():
+        UserLat = EnterLat.get()
+        UserLong = EnterLong.get()
+        print(UserLat)
+        print(UserLong)
+        float(UserLat)
+        float(UserLong)
+        userdf = pd.DataFrame({'Longitude': UserLong, 'Latitude':UserLat}, index=[0])
+        gdf = gpd.GeoDataFrame(userdf, geometry=gpd.points_from_xy(userdf.Longitude, userdf.Latitude, crs=myCRS))
+        gdf.plot(ax=ax, marker='o', color='blue', markersize=7)
 
-#Displays the map
+        plt.show()
+
+    window.title('Manual Entry')
+    LatLongIntro = tkk.Label(window, text='Enter Lat/Long coordinates below')
+    tkk.Label(window,text='Latitude: ').grid( column=0, row=0, )
+    tkk.Label(window, text='Longitude: ').grid( column=0, row=1)
+    EnterLat= tkk.Entry(window)
+    EnterLat.grid(column=1, row=0)
+    EnterLong= tkk.Entry(window)
+    EnterLong.grid(column=1, row=1)
+    submitLaLo = tkk.Button(window, text='Submit', command=genMapLaLo).grid(columnspan=2)
+    window.mainloop()
+    root.destroy()
+    return gdf
+
+
+#----------3.0 User Prompt GUI----------#
+root = tkk.Tk() #Establish intro Screen
+root.title('welcome')
+#intro text for screen1 of dialogue tree
+introText = tkk.Label(root, text='Welcome to the nearest Mcdonalds locator. You can choose to find the nearest McDonalds in Northern Ireland from your location, or enter custom Lat/Long coordinates')
+
+#Buttons to choose between Geolocation or custom lat/long
+uGLButt = tkk.Button(root, text="My Location", command=userGeoLoc)
+LaLoButt = tkk.Button(root, text="Enter Lat/Long", command=userManualEntry)
+
+introText.grid(columnspan=2)
+uGLButt.grid(row=1, column=0)
+LaLoButt.grid(row=1, column=1)
+
+root.mainloop()
+#----------Distance calculation----------#
